d8ef60f9f08bd01d6d12ab38d334c867c521809d
rendy <rendy.faqot@gmail.com>
2025-05-18 17:22:44 +0700

    chore: update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :sleep_records
+  has_many :follows, foreign_key: :follower_id
+  has_many :following, through: :follows, source: :followed
+  has_many :reverse_follows, class_name: 'Follow', foreign_key: :followed_id
+  has_many :followers, through: :reverse_follows, source: :follower
 end
